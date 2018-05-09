f0f3397b7a725a2e6af991e1795bedb99ced0546
Kijko <kijkongs@gmail.com>
2018-05-09 23:23:04 +0200

    clean

--- refueling-logger/src/main/java/kijko/web/RefuelingLoggerApplication.java
+++ refueling-logger/src/main/java/kijko/web/RefuelingLoggerApplication.java
@@ -1,16 +1,7 @@
 package kijko.web;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import kijko.web.models.Vehicle;
-import kijko.web.models.VehicleFactory;
-import kijko.web.models.enums.FuelType;
-import kijko.web.repository.CarRepository;
-import kijko.web.repository.VehicleRepository;
 
 @SpringBootApplication
 public class RefuelingLoggerApplication {
@@ -19,8 +10,4 @@ public class RefuelingLoggerApplication {
 		SpringApplication.run(RefuelingLoggerApplication.class, args);
 	}
 	
-	@PostConstruct
-	public void ee() {
-
-	}
 }
